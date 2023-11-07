3158ade19a4d98417621098bfd1ba1b0e2b3afbc
Álex Maciel <aweasley.gina@gmail.com>
2023-11-07 18:01:29 -0300

    created theme global of project using material ui

---
+++ src/components/ThemeRegistry/theme.ts
@@ -0,0 +1,23 @@
+import { Roboto } from 'next/font/google';
+import { createTheme } from '@mui/material/styles';
+
+const roboto = Roboto({
+  weight: ['300', '400', '500', '700'],
+  subsets: ['latin'],
+  display: 'swap',
+});
+
+const theme = createTheme({
+  palette: {
+    primary :{
+        main: '#235558',
+        light: '#e7e7e7',
+        dark: '#0E0E0E',
+    },
+  },  
+  typography: {
+    fontFamily: roboto.style.fontFamily,
+  }
+});
+
+export default theme;
